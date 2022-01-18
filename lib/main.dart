f2b40dff4ad17a91db7a5b7ee8cb6de83ac05e63
Raphael Pizzo <raphael.pizzo@pizzosoft.com.br>
2022-01-18 09:17:55 -0300

    Instalacao do pacote intl e formatando a data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -78,7 +79,7 @@ class MyHomePage extends StatelessWidget {
                           textAlign: TextAlign.start,
                         ),
                         Text(
-                          tr.date.toString(),
+                          DateFormat('d MMM y').format(tr.date),
                           style: TextStyle(color: Colors.grey),
                         ),
                       ],
